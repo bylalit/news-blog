d3b2bfdc7ecd7ea33e66a3634147d5177fb9d72d
Lalit kumar Prajapati <146929824+bylalit@users.noreply.github.com>
2025-08-13 10:25:29 +0530

    Delete middleware directory

--- middleware/isAdmin.js
+++
@@ -1,9 +0,0 @@
-const isAdmin = (req, res, next) => {
-    if (req.role === 'admin') {
-        next();
-    }else{
-        res.redirect('/admin/dashboard/');
-    }
-}
-
-module.exports = isAdmin;
